3f7cd17f3256a0370aec131481acddd51a6261da
Potato <512006634@qq.com>
2018-05-25 21:03:30 +0800

    数据修改

--- src/app/pages/index/index.service.ts
+++ src/app/pages/index/index.service.ts
@@ -22,7 +22,67 @@ export class IndexService{
         'assets/Chapter10/10-1.5.jpg',
       ]
     },
-    {chapter:'第十一章 井流公式',des:'地表径流',
+    {chapter:'第十一章 孔隙水',des:'洪积扇的地下水',
+      urls:[
+        'assets/Chapter10/chapter10.jpg',
+        'assets/Chapter10/10-1.1.jpg',
+        'assets/Chapter10/10-1.2.jpg',
+        'assets/Chapter10/10-1.3.jpg',
+        'assets/Chapter10/10-1.4.jpg',
+        'assets/Chapter10/10-1.5.jpg',
+      ]
+    },
+    {chapter:'第十二章 裂隙水',des:'裂隙水的类型',
+      urls:[
+        'assets/Chapter10/chapter10.jpg',
+        'assets/Chapter10/10-1.1.jpg',
+        'assets/Chapter10/10-1.2.jpg',
+        'assets/Chapter10/10-1.3.jpg',
+        'assets/Chapter10/10-1.4.jpg',
+        'assets/Chapter10/10-1.5.jpg',
+      ]
+    },
+    {chapter:'第十三章 熔岩水',des:'熔岩发育的基本条件',
+      urls:[
+        'assets/Chapter10/chapter10.jpg',
+        'assets/Chapter10/10-1.1.jpg',
+        'assets/Chapter10/10-1.2.jpg',
+        'assets/Chapter10/10-1.3.jpg',
+        'assets/Chapter10/10-1.4.jpg',
+        'assets/Chapter10/10-1.5.jpg',
+      ]
+    },
+    {chapter:'第十四章 地下水资源',des:'地下水分类及供水意义',
+      urls:[
+        'assets/Chapter10/chapter10.jpg',
+        'assets/Chapter10/10-1.1.jpg',
+        'assets/Chapter10/10-1.2.jpg',
+        'assets/Chapter10/10-1.3.jpg',
+        'assets/Chapter10/10-1.4.jpg',
+        'assets/Chapter10/10-1.5.jpg',
+      ]
+    },
+    {chapter:'第十五章 地下水与环境',des:'生态环境系统的特性',
+      urls:[
+        'assets/Chapter10/chapter10.jpg',
+        'assets/Chapter10/10-1.1.jpg',
+        'assets/Chapter10/10-1.2.jpg',
+        'assets/Chapter10/10-1.3.jpg',
+        'assets/Chapter10/10-1.4.jpg',
+        'assets/Chapter10/10-1.5.jpg',
+      ]
+    },
+    {chapter:'第十六章 水文地质学研究方法',des:'水文地质学发展趋势',
+      urls:[
+        'assets/Chapter10/chapter10.jpg',
+        'assets/Chapter10/10-1.1.jpg',
+        'assets/Chapter10/10-1.2.jpg',
+        'assets/Chapter10/10-1.3.jpg',
+        'assets/Chapter10/10-1.4.jpg',
+        'assets/Chapter10/10-1.5.jpg',
+      ]
+    },
+    {chapter:'第十七章 井流公式',des:'地表径流',
       urls:[
         'assets/Chapter11/chapter11.jpg',
       ]
--- src/app/pages/teacher/teach-plan/teach-plan.service.ts
+++ src/app/pages/teacher/teach-plan/teach-plan.service.ts
@@ -4,10 +4,10 @@ import {planItemModel} from "../../components/plan/plan.component";
 @Injectable()
 export class TeachPlanService{
   plans:Array<planItemModel>=[
-    {id:1,StartTime:new Date(2018,3,7,17,22),EndTime:new Date(2018,3,7,17,55),text:'begin first chapter',statu:'finished'},
-    {id:2,StartTime:new Date(2018,3,8,12,22),EndTime:new Date(2018,3,7,16,25),text:'begin second chapter',statu:'finished'},
-    {id:3,StartTime:new Date(2018,3,8,12,22),EndTime:new Date(2018,3,7,16,25),text:'begin second chapter',statu:'doing'},
-    {id:4,StartTime:new Date(2018,3,8,12,22),EndTime:new Date(2018,3,7,16,25),text:'begin second chapter',statu:'undone'},
+    {id:1,StartTime:new Date(2018,3,7,17,22),EndTime:new Date(2018,3,7,17,55),text:'提醒12班交作业',statu:'finished'},
+    {id:2,StartTime:new Date(2018,3,8,12,22),EndTime:new Date(2018,3,7,16,25),text:'今天讲到第九章地下水类型',statu:'finished'},
+    {id:3,StartTime:new Date(2018,3,8,12,22),EndTime:new Date(2018,3,7,16,25),text:'下课点名',statu:'doing'},
+    {id:4,StartTime:new Date(2018,3,8,12,22),EndTime:new Date(2018,3,7,16,25),text:'带上教学用具',statu:'undone'},
   ];
   constructor(){
   }
