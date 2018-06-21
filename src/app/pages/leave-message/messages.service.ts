264af6a08a85eab6daf1f955d8fc80846c45ac00
Potato <512006634@qq.com>
2018-06-21 23:21:19 +0800

    数据修改2

--- src/app/pages/leave-message/messages.service.ts
+++ src/app/pages/leave-message/messages.service.ts
@@ -3,9 +3,9 @@ import {Injectable} from "@angular/core";
 @Injectable()
 export class MessagesService{
   messagesData:Array<mesDataModel>=[
-    {user:'Test1',color:'red',messages:['aaaaa'],time:new Date(2018,2,13,15,23)},
-    {user:'User1',color:'green',messages:['bbbb'],time:new Date(2018,2,13,16,23)},
-    {user:'User1',color:'green',messages:['cccc'],time:new Date(2018,2,14,11,55)},
+    {user:'小明',color:'red',messages:['想要一份外文文献，有推荐吗'],time:new Date(2018,2,13,15,23)},
+    {user:'小红',color:'green',messages:['如何分析地下水的形成'],time:new Date(2018,2,13,16,23)},
+    {user:'小李',color:'#0e77ca',messages:['最迟什么时候交作业'],time:new Date(2018,2,14,11,55)},
   ];
 }
 export class mesDataModel{
--- src/app/pages/teacher/homework/homework.service.ts
+++ src/app/pages/teacher/homework/homework.service.ts
@@ -7,19 +7,28 @@ export class HomeworkService{
       id:1,
       active: false,
       disabled: false,
-      name: '作业一',
+      name: '简述地下水化学成分的形成与变化规律',
       time:new Date(2018,2,20,17,19,0),
-      content:'作业一的内容',
-      attachment:'这是作业一的附件'
+      content:'包括水化学成分形成的各种因素、水化学成分形成的各种物理化学（包括离子、气体、有机物、微生物及同位素成分）的作用、水中各种元素的来源',
+      attachment:'attachment.rar'
     },
     {
       id:2,
       active: false,
       disabled: true,
-      name: '作业二',
+      name: '地下水中各种元素的迁移作用',
       time:new Date(2018,2,21,17,19,0),
-      content:'作业二的内容',
-      attachment:'这是作业二的附件'
+      content:'如元素从矿物和岩浆、气体、生命等物质转移到地下水的过程，元素进入地下水后由于各种因素引起的迁移作用。元素的迁移作用是水文地球化学分带及地下水化学成分变化的制约条件',
+      attachment:'attachment.rar'
+    },
+    {
+      id:2,
+      active: false,
+      disabled: true,
+      name: '人类活动对地下水的影响',
+      time:new Date(2018,2,23,17,19,0),
+      content:'人类的经济技术活动造成水文地球化学异常，并对环境和生态产生重大影响。请查阅资料并举例',
+      attachment:'attachment.rar'
     }
   ];
   fileList = [{
