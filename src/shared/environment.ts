1b4e56b8674c6599d6f15ce1ce9295acf819b5d7
PotatoGG <512006634@qq.com>
2018-06-22 18:35:55 +0800

    dataChange

--- src/shared/environment.ts
+++ src/shared/environment.ts
@@ -2,8 +2,8 @@ import {Injectable} from "@angular/core";
 
 @Injectable()
 export class Environment{
-  loginUrl:string='http://localhost:80/server/login';
-  registerUrl:string='http://localhost:80/server/register';
-  uploadPDF:string='http://localhost:80/server/upload';
-  uploadHomework:string='http://localhost:80/server/uploadHomework';
+  loginUrl:string='http://localhost:8080/server/login';
+  registerUrl:string='http://localhost:8080/server/register';
+  uploadPDF:string='http://localhost:8080/server/upload';
+  uploadHomework:string='http://localhost:8080/server/uploadHomework';
 }
