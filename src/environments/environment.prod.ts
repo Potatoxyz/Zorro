f65adb43924a84b70e6ff32bb0268fe9eec35a37
陈晖 <512006634@qq.com>
2018-07-18 18:29:52 +0800

    生产路径

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  loginUrl: 'http://localhost:80/server/login',
-  registerUrl: 'http://localhost:80/server/register',
-  uploadPDF: 'http://localhost:80/server/upload',
-  uploadHomework: 'http://localhost:80/server/uploadHomework',
+  loginUrl: 'http://45.77.165.65:80/server/login',
+  registerUrl: 'http://45.77.165.65:80/server/register',
+  uploadPDF: 'http://45.77.165.65:80/server/upload',
+  uploadHomework: 'http://45.77.165.65:80/server/uploadHomework',
 };
