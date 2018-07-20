0b476d7768ae9c44a42524f366023236abe74b4e
陈晖 <512006634@qq.com>
2018-07-20 16:56:33 +0800

    ...

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  loginUrl: 'http://45.77.165.65:4900/server/login',
-  registerUrl: 'http://45.77.165.65:4900/server/register',
-  uploadPDF: 'http://45.77.165.65:4900/server/upload',
-  uploadHomework: 'http://45.77.165.65:4900/server/uploadHomework',
+  loginUrl: 'http://45.77.165.63:4900/server/login',
+  registerUrl: 'http://45.77.163.65:4900/server/register',
+  uploadPDF: 'http://45.77.165.63:4900/server/upload',
+  uploadHomework: 'http://45.77.165.63:4900/server/uploadHomework',
 };
